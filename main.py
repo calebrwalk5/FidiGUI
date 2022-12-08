d4f24fc05c245e232c0eab7c2176d3b1bffbb23c
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-12-08 12:18:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,24 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import * 
 
-# this is the function called when the button is clicked
 def goFunction():
 	print('clicked')
 
-
-# this is a function to get the user input from the text input box
 def getInputBoxValue():
 	userInput = Prompt.get()
 	return userInput
 
-
-
 root = Tk()
 
-# This is the section of code which creates the main window
 root.geometry('830x430')
 root.configure(background='#FFFFFF')
 root.title('FidiGUI')
 
+Button(root, text='GO', bg='#00FF00', font=('arial', 12, 'normal'), command=goFunction).place(x=7, y=1)
 
-# This is the section of code which creates a button
-Button(root, text='GO', bg='#000000', font=('arial', 12, 'normal'), command=goFunction).place(x=7, y=1)
-
-
-# This is the section of code which creates a text input box
 Prompt=Entry(root)
 Prompt.place(x=57, y=1)
 
-
-# This is the section of code which creates the a label
 Label(root, text='FidiGUI', bg='#FFFFFF', font=('arial', 12, 'normal')).place(x=57, y=21)
 
 
