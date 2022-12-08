513180e787146be5e6c1e58ee9a30c398c4c2cde
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-12-08 12:23:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from tkinter import ttk
 from tkinter import * 
 
 def goFunction():
-	print('clicked')
+	print(userInput)
 
 def getInputBoxValue():
 	userInput = Prompt.get()
+	print(userInput)
 	return userInput
+
+def printInput():
+    inp = inputtxt.get(1.0, "end-1c")
+    lbl.config(text = "Provided Input: "+userInput)
 
 root = Tk()
 
@@ -15,12 +20,12 @@ root.geometry('830x430')
 root.configure(background='#FFFFFF')
 root.title('FidiGUI')
 
-Button(root, text='GO', bg='#00FF00', font=('arial', 12, 'normal'), command=goFunction).place(x=7, y=1)
+Button(root, text='GO', bg='#00FF00', font=('arial', 12, 'normal'), command=getInputBoxValue).place(x=7, y=1)
 
 Prompt=Entry(root)
 Prompt.place(x=57, y=1)
 
-Label(root, text='FidiGUI', bg='#FFFFFF', font=('arial', 12, 'normal')).place(x=57, y=21)
+Label(root, text='Prompt', bg='#FFFFFF', font=('arial', 12, 'normal')).place(x=57, y=21)
 
 
 root.mainloop()
