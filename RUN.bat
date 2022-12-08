2ea5da2c6d795fbc79bb28d7f5779e7ccb3c9bda
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-12-08 12:31:32 -0500

    Delete RUN.bat

--- RUN.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-color 0a
-set /p Input=Enter Text (one word only for now): 
-python image-from-text.py --text=%Input% --no-mega
-python upscale.py 2 generated.png bilinear
-del generated.png
-echo upscaled!
-pause
