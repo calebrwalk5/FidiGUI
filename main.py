e96e9cd6f359fa74c434129503b85d0995ac3dc3
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-12-10 18:36:13 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -16,22 +16,25 @@ def printInput():
     inp = inputtxt.get(1.0, "end-1c")
     lbl.config(text = "Provided Input: "+userInput)
 
+def showImage():
+	path = "generated.png"
+	img = ImageTk.PhotoImage(Image.open(path))
+	panel = Label(root, image=img)
+	panel.photo = img
+	panel.pack(side = "bottom", expand = "yes")
+
 root = Tk()
 
 root.geometry('830x430')
 root.configure(background='#808080')
 root.title('FidiGUI')
 
-img = ImageTk.PhotoImage(Image.open("generated.png"))
-panel = Label(root, image = img)
-panel.pack(side = "bottom", fill = "both", expand = "yes")
-
 Button(root, text='GO', bg='#00FF00', font=('arial', 12, 'normal'), command=getInputBoxValue).place(x=7, y=1)
+
+Button(root, text='show image', bg='#00FFFF', font=('arial', 12, 'normal'), command=showImage).place(x=7, y=30)
 
 Prompt=Entry(root)
 Prompt.place(x=57, y=1)
-
-Label(root, text='Prompt', bg='#FFFFFF', font=('arial', 12, 'normal')).place(x=57, y=21)
 
 
 root.mainloop()
